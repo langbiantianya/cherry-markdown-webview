d4c6cc8285bfccb90b225d9447088fc6b76d3fa9
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2024-12-06 14:50:27 +0800

    first commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,125 @@
+<script>
+  // import {Greet} from '../wailsjs/go/main/App.js'
+  import { onMount } from "svelte";
+  import "cherry-markdown/dist/cherry-markdown.css";
+  import Cherry from "cherry-markdown";
+
+  onMount(() => {
+    /**
+     * 定义一个空壳，用于自行规划cherry已有工具栏的层级结构
+     */
+    var fileMenu = Cherry.createMenuHook("文件", {});
+
+    var openFileMenu = Cherry.createMenuHook("打开文件", {
+      onClick: async () => {
+        const [fileHandle] = await window.showOpenFilePicker({
+          types: [
+            {
+              accept: {
+                "text/plain": [".md", ".mdx"],
+              },
+            },
+          ],
+          multiple: true,
+        });
+        var reader = new FileReader(); // 创建FileReader对象
+        const fileData = await fileHandle.getFile();
+        console.log(fileData);
+        reader.readAsText(fileData);
+        reader.onload = function () {
+          cherryInstance.setMarkdown(this.result);
+        };
+      },
+    });
+    const cherryInstance = new Cherry({
+      id: "markdown-container",
+      value: "",
+      toolbars: {
+        // 定义顶部工具栏
+        toolbar: [
+          { fileMenu: ["openFileMenu"] },
+          "|",
+          "undo",
+          "redo",
+          "|",
+          // 把字体样式类按钮都放在加粗按钮下面
+          {
+            bold: [
+              "bold",
+              "italic",
+              "underline",
+              "strikethrough",
+              "sub",
+              "sup",
+              "ruby",
+            ],
+          },
+          "color",
+          "size",
+          "|",
+          "quote",
+          "detail",
+          "header",
+          "ul",
+          "ol",
+          "checklist",
+          "list",
+          "justify",
+          "panel",
+          "|",
+          "code",
+          // 把插入类按钮都放在插入按钮下面
+          {
+            insert: [
+              "image",
+              "audio",
+              "video",
+              "pdf",
+              "word",
+              "file",
+              "link",
+              "hr",
+              "br",
+              "formula",
+              "toc",
+              "table",
+              "drawIo",
+            ],
+          },
+          "graph",
+        ],
+        // 定义侧边栏，默认为空
+        sidebar: ["theme", "mobilePreview", "copy"],
+        // 定义顶部右侧工具栏，默认为空
+        toolbarRight: ["export", "togglePreview"],
+        // 定义选中文字时弹出的“悬浮工具栏”，默认为 ['bold', 'italic', 'underline', 'strikethrough', 'sub', 'sup', 'quote', '|', 'size', 'color']
+        bubble: [
+          "color",
+          "size",
+          "|",
+          "bold",
+          "italic",
+          "underline",
+          "strikethrough",
+          "|",
+          "quote",
+          "header",
+          "ul",
+          "ol",
+          "checklist",
+        ],
+        // 定义光标出现在行首位置时出现的“提示工具栏”，默认为 ['h1', 'h2', 'h3', '|', 'checklist', 'quote', 'table', 'code']
+        float: ["table", "code", "graph"],
+        customMenu: {
+          fileMenu: fileMenu,
+          openFileMenu: openFileMenu,
+        },
+      },
+    });
+  });
+</script>
+
+<div id="markdown-container" class="h-full"></div>
+
+<style>
+</style>
