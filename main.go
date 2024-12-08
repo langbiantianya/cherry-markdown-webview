fb66d7dfb121344c7437af6370a4d3cadd55befd
langbiantianya <langbiantianya@outlook.com>
2024-12-08 12:54:01 +0800

    fix: disable Windows Pinch&  Mouse Scroll Zoom

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -17,6 +18,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
+		Windows: &windows.Options{
+			IsZoomControlEnabled: false,
+		},
+
 		Title:  "cherry-markdown-webview",
 		Width:  1024,
 		Height: 600,
